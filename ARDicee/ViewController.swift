92655ece3f596a1cd6f56e503c432bb6a1c72387
DanielY1108 <scarlet040@gmail.com>
2023-01-06 17:45:13 +0900

    우주배경 달만들기

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -12,7 +12,7 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,18 +22,52 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        makeSphere()
         
-        // Set the scene to the view
-        sceneView.scene = scene
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
     }
+    
+    func makeSphere() {
+        // 구 모양을 생성한다.
+        let sphere = SCNSphere(radius: 0.2)
+        
+        let material = SCNMaterial()
+        
+        // 추가한 texture 파일을 UIImage로 불러오기만 하면된다.
+        // art.scnassets 폴더 안에 있으므로 경로를 추가하여 넣어준다.
+        material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpeg")
+        
+        sphere.materials = [material]
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(x: 0, y: 0.05, z: -1)
+        
+        node.geometry = sphere
+       
+        // 배경 추가하기
+        sceneView.scene.background.contents = UIImage(named: "art.scnassets/space.jpeg")
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
+
+    }
+
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        // 나의 기기가 지원하는지 알수 있습니다. (요즘은 대부분 iPhoneSE 이상을 사용하므로 딱히 분기처리를 하여 사용할 필요는 없을 듯 싶다)
+        print("Orientation Tracking is supported = \(ARConfiguration.isSupported)")
+        print("World Tracking is supported = \(ARWorldTrackingConfiguration.isSupported)")
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -46,29 +80,4 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
